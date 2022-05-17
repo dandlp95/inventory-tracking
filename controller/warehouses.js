ce259dfd4414b569502f74981355ab0115322d3d
dandlp95 <del17011@byui.edu>
2022-05-17 11:26:48 -0600

    addProductWarehouse almost functionality/logic finished

--- controller/warehouses.js
+++ controller/warehouses.js
@@ -1,4 +1,5 @@
 const warehouseModel = require("../db/warehouseModel");
+const productModel = require("../db/productModel");
 const mongoose = require("mongoose");
 
 const getAllWarehouses = async function (req, res) {
@@ -14,6 +15,15 @@ const getAllWarehouses = async function (req, res) {
 const getWarehouse = async function (req, res) {
   const warehouse = await warehouseModel.findById(req.params.id);
 
+  // temporary debugging code below.
+  // const product = await warehouseModel
+  //   .findById(req.params.id)
+  //   .populate("inventory.product");
+
+  // const item = product.inventory[0].product;
+  // item.description = "I hate cheese. EDITED";
+  // item.save();
+
   try {
     res.status(200).send(warehouse);
   } catch (err) {
@@ -23,21 +33,61 @@ const getWarehouse = async function (req, res) {
 
 const addProductWarehouse = async function (req, res) {
   const objectId = mongoose.Types.ObjectId(req.params.productId);
+  const qtyAdded = req.body.quantity; // Quantity added to inventory
+  const product = await productModel.findById(req.params.productId);
+  const totalQty = product.quantity;
 
-  const addedInventory = {
-    product: objectId,
-    quantity: req.body.quantity,
-  };
+  if (qtyAdded > totalQty) {
+    res.send("Insufficient Inventory.");
+  } else {
+    product.quantity = totalQty - qtyAdded; // Removes qty from inventory.
+    product.save();
 
-  const warehouse = await warehouseModel.findById(req.params.id);
-  warehouse.inventory.push(addedInventory);
+    // Object added to inventory.
+    // product references product in products collection.
+    const addedInventory = {
+      product: objectId,
+      quantity: qtyAdded
+    };
 
-  try {
+    // pushes object to inventory list in warehouse.
+    const warehouse = await warehouseModel.findById(req.params.id);
+    warehouse.inventory.push(addedInventory);
     await warehouse.save();
-    res.status(200).send("Product added.");
-  } catch (err) {
-    res.status(500).send(err);
+
+    try{
+      res.status(200).send(`Product Added succesfully: ${product}`);
+    }catch(err){
+      res.status(500).send(err);
+    }
   }
+
+  // await warehouse.save();
+
+  // const product = await productModel.findById(req.params.productId);
+
+  // const totalQty = product.quantity;
+
+  // if (qty > totalQty) {
+  //   res.send("Insufficient Inventory");
+  // } else {
+  //   product.quantity = totalQty - qtyAdded;
+  //   await product.save();
+  // }
+
+  // const addedInventory = {
+  //   product: objectId,
+  //   quantity: qtyAdded,
+  // };
+
+  // const warehouse = await warehouseModel.findById(req.params.id);
+  // warehouse.inventory.push(addedInventory);
+
+  // try {
+  //   res.status(200).send("Product added.");
+  // } catch (err) {
+  //   res.status(500).send(err);
+  // }
 };
 
 module.exports = { getAllWarehouses, getWarehouse, addProductWarehouse };
