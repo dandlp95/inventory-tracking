6d807e840cc8fa5404d78fb0f5bf004cbb5dacb3
dandlp95 <del17011@byui.edu>
2022-05-21 18:40:55 -0600

    prepare for deployment

--- index.js
+++ index.js
@@ -4,15 +4,23 @@ const cors = require("cors");
 const app = express();
 require("dotenv").config();
 const PORT = process.env.PORT || 8080;
+const path = require("path");
 
 mongoose.connect(process.env.MONGO_URI).then(() => {
   app.use(cors()).use(express.json()).use("/", require("./routes"));
 
-  if (process.env.NODE_ENV === "production") {
-      
+  if (process.env.NODE_ENV === "production"){
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+      res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+
+    })
+
   }
 
   app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
   });
 });
+
